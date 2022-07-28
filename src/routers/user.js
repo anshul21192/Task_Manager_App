937c7ef8d3c33903f41d5c9af56d651864c533c3
anshul21192 <83779699+anshul21192@users.noreply.github.com>
2022-07-28 19:59:20 +0530

    Third commit

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,7 @@ const User=require('../models/user');
 const auth=require('../middleware/auth');
 const multer=require('multer');
 const sharp=require('sharp');
-const {sendWelcomeEmail,sendGoogByeEmail}=require('../emails/account');
+// const {sendWelcomeEmail,sendGoogByeEmail}=require('../emails/account');
 
 const router=new express.Router();
 
@@ -12,7 +12,7 @@ router.post('/users', async (req,res)=>{
     const user=new User(req.body);
     try{
         await user.save();
-        sendWelcomeEmail(user.email,user.name);
+//         sendWelcomeEmail(user.email,user.name);
         const token=await user.generateToken();
         res.status(201).send({user,token});
     }catch(e){
@@ -74,7 +74,7 @@ router.patch('/users/me', auth ,async (req,res)=>{
 router.delete('/users/me', auth ,async (req,res)=>{
     try{
         await req.user.remove();
-        sendGoogByeEmail(req.user.email,req.user.name);
+//         sendGoogByeEmail(req.user.email,req.user.name);
         res.status(200).send(req.user);
     }catch(e){
         res.status(500).send(e);
